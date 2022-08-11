bdb4e628f2b6433ee92f1d4d31903648c59a633c
loica <loica.etudiant@gmail.com>
2022-08-11 10:14:21 +0300

    html template

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <div className="screen">
+          <div className="screen__content">
+            <form className="login">
+              <div className="login__field">
+                <i className="login__icon fas fa-user"></i>
+                <label>
+                  <input type="text" className="login__input" placeholder="User name / Email"></input>
+                </label>
+              </div>
+              <div className="login__field">
+                <i className="login__icon fas fa-lock"></i>
+                <label>
+                  <input type="password" className="login__input" placeholder="Password"></input>
+                </label>
+              </div>
+              <button className="button login__submit">
+                <span className="button__text">Log In Now</span>
+                <i className="button__icon fas fa-chevron-right"></i>
+              </button>
+            </form>
+            <div className="social-login">
+              <h3>log in via</h3>
+              <div className="social-icons">
+                <a href="#" className="social-login__icon fab fa-instagram"></a>
+                <a href="#" className="social-login__icon fab fa-facebook"></a>
+                <a href="#" className="social-login__icon fab fa-twitter"></a>
+              </div>
+            </div>
+          </div>
+          <div className="screen__background">
+            <span className="screen__background__shape screen__background__shape4"></span>
+            <span className="screen__background__shape screen__background__shape3"></span>
+            <span className="screen__background__shape screen__background__shape2"></span>
+            <span className="screen__background__shape screen__background__shape1"></span>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
